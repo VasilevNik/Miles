15c93d6d98f32581a5d2217b7b750fd4358baa1a
Nikolay Vasilev <lexuspexus@yandex.ru>
2022-12-02 18:19:27 +0300

    Change main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int price = 60; // стоимость билета
+        int spent = 20; // потраченная сумма за которую начисляется 1 миля
+        int bonus = (price / spent); // расчет миль
 
+        System.out.println("За купленный билет начислено - " + bonus + " мили(ль)");
     }
 }
